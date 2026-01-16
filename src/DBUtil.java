b17599cf5d50274e2f968b42927c42c80e161ae1
marlenzhai-wq <marlen.zhai@gmail.com>
2026-01-17 00:14:52 +0500

    вв

--- src/DBUtil.java
+++ src/DBUtil.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String URL = "jdbc:postgresql://localhost:5432/socialdb";
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
     private static final String PASSWORD = "123456"; // өзіңнің паролің
 

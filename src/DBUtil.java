536dec401671efbba6feccc5ac69c4df9a61eee9
marlenzhai-wq <marlen.zhai@gmail.com>
2026-01-21 12:14:35 +0500

    вв

--- src/DBUtil.java
+++ src/DBUtil.java
@@ -5,8 +5,7 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "123456"; // өзіңнің паролің
-
+    private static final String PASSWORD = "123456";
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
